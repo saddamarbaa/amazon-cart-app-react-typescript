f49db862f4577cc77d2821e98a85619ad7dd4c67
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-01-28 22:14:12 +0700

    conveted the project back to JSX

--- src/App.js
+++ src/App.js
@@ -3,38 +3,22 @@ import "./App.css";
 
 function App() {
   const firstName = "saddam";
-  return /*#__PURE__*/ React.createElement(
-    "div",
-    {
-      className: "App",
-    },
-    /*#__PURE__*/ React.createElement(
-      "header",
-      {
-        className: "App-header",
-      },
-      /*#__PURE__*/ React.createElement("img", {
-        src: logo,
-        className: "App-logo",
-        alt: "logo",
-      }),
-      /*#__PURE__*/ React.createElement(
-        "h2",
-        null,
-        "Hello my friends lets learn react js"
-      ),
-      /*#__PURE__*/ React.createElement(
-        "a",
-        {
-          className: "App-link",
-          href: "https://reactjs.org",
-          target: "_blank",
-          rel: "noopener noreferrer",
-        },
-        "Learn React with ",
-        firstName
-      )
-    )
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Hello my friends lets learn react js</h2>
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React with {firstName}
+        </a>
+      </header>
+    </div>
   );
 }
 
