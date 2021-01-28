86a68be66afb6c02752dfad483910b5af1f15e4b
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-01-28 22:11:53 +0700

    added my name to test the project

--- src/App.js
+++ src/App.js
@@ -2,21 +2,20 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const firstName = "saddam";
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Hello my friends lets learn react js</h2>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with {firstName}
         </a>
       </header>
     </div>
