58b946c26a5a31a0047b611240d108510a0f5685
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-01-31 19:41:02 +0700

    ADD:CartItems from Data.JS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import CartItems from "./components/CartItems";
 import CartTotal from "./components/CartTotal";
+import data from "./Data";
 import "./App.css";
 
 /*********************** */
@@ -12,11 +13,16 @@ import Temp from "./Temp";
 // App component(parent component)
 
 const App = () => {
+  // Declare a new state variable, which we'll call "cartItems"
+  const [cartItems, setCartItems] = useState(data);
+  // console.log(data);
+  // console.log(cartItems);
+
   return (
     <div className="App">
       <Header title="Amazon Cart" />
       <div className="App-main">
-        <CartItems title="Shopping Cart" />
+        <CartItems items={cartItems} />
         <CartTotal title="Cart Total" />
       </div>
 
--- src/components/CartItems.js
+++ src/components/CartItems.js
@@ -4,10 +4,23 @@ import "./CartItems.css";
 
 // CartItems component(child component)
 
-const CartItems = ({ title }) => {
+const CartItems = (items) => {
+  console.log("Inside CartItems component ", items);
+  for (const key in items) {
+    // const element = object[key];
+    // console.log(items[key]);
+    let x = items[key];
+    // console.log(x);
+    x.forEach((t) => {
+      console.log(t.title);
+    });
+  }
+
+  // const { title, stock, image, price, quantity } = props;
+
   return (
     <div className="CartItems">
-      <h1>{title}</h1>
+      <h1>Shopping Cart</h1>
       <hr />
       <div className="CartItems-items">
         <CartItem />
