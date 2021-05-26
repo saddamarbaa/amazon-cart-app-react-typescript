a9d02402875a5972d2924d44676d46babf41e87d
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-05-26 22:56:26 +0700

    add:  firebase changes and last deployed link

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+/** @format */
+
+const firebaseConfig = {
+	apiKey: "AIzaSyAY6NvPcVo3xL0raBz7gvqVRgsoKp5J-QI",
+	authDomain: "cart-react-576c5.firebaseapp.com",
+	projectId: "cart-react-576c5",
+	storageBucket: "cart-react-576c5.appspot.com",
+	messagingSenderId: "526115908000",
+	appId: "1:526115908000:web:30a5a8e94b60348efd9cc5",
+};
+
+
+// deployed link
+// https://cart-react-576c5.web.app/
